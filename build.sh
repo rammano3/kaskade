bb562be19a759379065207061c6a00dcab809096
Alexander Martin <alex@suitupalex.com>
2015-02-27 13:23:23 -0600

    Updated build.sh to work with salesforce.

--- build.sh
+++ build.sh
@@ -2,6 +2,21 @@
 
 for d in $(find .. -name 'pom.xml' | grep -v 'node_modules' | sed 's/\/pom\.xml//g'); do
 cd $d
+
+if [ -f install_salesforce_jars_to_maven.sh ]; then
+  cat <<EOF
+
+_____________________________________
+
+ INSTALLING SALESFORCE JARS TO MAVEN
+_____________________________________
+
+
+EOF
+
+  ./install_salesforce_jars_to_maven.sh
+fi
+
 echo "Maven build on $d";
 mvn
 if [ -f install_esb_jar_to_maven.sh ]; then
@@ -17,6 +32,7 @@ EOF
 
   ./install_esb_jar_to_maven.sh
 fi
+
 cd -; done
 
 for d in $(find .. -name 'package.json' | grep -v 'node_modules' | sed 's/\/package\.json//g'); do
